94dfb904eb667cee7d0d6ac4761204ef84f87f55
fahrulf14 <46362032+fahrulf14@users.noreply.github.com>
2021-09-24 13:41:38 +0700

    Prepare to Split Area

--- Startup.cs
+++ Startup.cs
@@ -75,6 +75,10 @@ namespace NUNA
                     pattern: "{controller=Home}/{action=Index}/{id?}");
 
                 endpoints.MapControllerRoute(
+                    name: "MyArea",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                      name: "defaultWithoutAction",
                      pattern: "{controller}/{id?}",
                      defaults: new { action = "Index" },
